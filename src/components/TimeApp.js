505e8ee3c52e714d13d6b18c9eddc9c7f688b09c
Krzysztofowi bojarczuk <krzysztofbojarczuk@wp.pl>
2021-11-03 09:14:17 +0100

    nauka

---
+++ src/components/TimeApp.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+
+
+const TimeApp = () => {
+    const cTime
+
+    const [state, setstate] = useState(initialState)
+
+
+    return (
+        <React.Fragment>
+            <section>
+                <div className="bg-black w-96 h-96 mt-20 mx-auto rounded-full text-white flex justify-center items-center">
+                    <div>
+                        <div>
+                            <p>day</p>
+
+                        </div>
+
+                        <div>
+                            <p>time</p>
+
+                        </div>
+
+                        <div>
+                            <p>date</p>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default TimeApp
